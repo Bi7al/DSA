aee0445aa4830f4e604bb98e06884d9062550f79
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-01-16 22:39:23 +0500

    Simplified Using python specific Tuple packing,unpacking

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,10 +1,13 @@
 array = [1,3,4,5,68,3,45,56,73];
+
+
+
+
+
 print("Original array: ", array);
 for i in range(len(array)-1):
     for j in range(i+1,len(array)-1):
         if array[i]>array[j]:
-            temp = array[j];
-            array[j]=array[i];
-            array[i]=temp
+            array[i],array[j]= array[j],array[i]; # Tuple Packing and Unpacking
 
 print("Sorted array: ", array);
