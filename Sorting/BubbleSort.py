b920fb40a05980ef0f3cca28d617c8893cbba656
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-01-18 23:51:58 +0500

    Optimized Bubble sort

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -6,10 +6,14 @@ array = [9,8,7,6,5,4,3,2,1];
 
 print("Original array: ", array);
 for i in range(len(array)):
+    swapped=False;
     for j in range(0,len(array)-1):
         if array[j]>array[j+1]:
             temp = array[j];
             array[j] = array[j+1];
             array[j+1] = temp;
+            swapped=True;
+    if not swapped:
+        break;
 
 print("Sorted array: ", array);
