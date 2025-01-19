d1279ae5d749b30fb80aac352969c5943159452c
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-01-20 00:31:28 +0500

    QuickSort Implemented

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -2,23 +2,27 @@
 #::: if currval == swapvalue  : pass
 #::: if currVal > Swap : Swap values
 # swap swap values with  pivot value
-def quicksort(array, start=0,high="none"):
-    if(start == high):
+def quicksort(array, start,high):
+    if(len(array)==1 or start >= high):
         return array;
     else:
-        length = len(array);
-        swap_Index = start;
-        pivot_value = array[high];
+        swap_Index = start-1;
+        pivot_value =  array[high];
         for current_Index in range(start,high+1):
             if array[current_Index]<=pivot_value:
+                swap_Index+=1;
                 if current_Index > swap_Index :
                     swap = array[swap_Index];
                     array[swap_Index] = array[current_Index];
                     array[current_Index] = swap; 
-                    
-                       
                 
-                ++swap_Index;
-            if array[current_Index]> pivot_value:
+            elif array[current_Index]> pivot_value:
                 pass;
+            
+        quicksort(array,start,swap_Index-1);
+        quicksort(array,swap_Index+1,high);
+        
+array = [9,8,7,6,5,4,3,2,1,0];
+quicksort(array,0,len(array)-1);
+print(array)
                 
